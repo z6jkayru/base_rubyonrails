8de57b5aee3460d78c82adcbcd9a7d2d3da3cf12
al3xisvg <vasquezgarciaalexis@gmail.com>
2022-09-10 14:15:02 -0500

    [udpate] before action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
+  # before_action :find_user, only: [:edit, :show, :update, :destroy]
+  before_action :find_user, except: [:new, :create]
+
   def new
     @user = User.new
     @user.email = "jhon.doe@gmail.com"
   end
 
   def edit
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
   end
 
   def create
@@ -18,11 +21,11 @@ class UsersController < ApplicationController
 
   def show
     puts "Estas entrando a mostrar..."
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
     @user.update(email: params[:user][:email])
 
     puts "\n\n\n #{@user.email} \n\n\n"
@@ -33,11 +36,15 @@ class UsersController < ApplicationController
   def destroy
     puts "Estas entrando a eliminar..."
 
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
     @user.destroy
 
     puts "\n\n\n #{@user} \n\n\n"
 
     redirect_to root_path
+  end
+
+  def find_user
+    @user = User.find(params[:id])
   end
 end
