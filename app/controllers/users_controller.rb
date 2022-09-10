495bc0d8411415322d30b83ca6d8fe29372f3261
al3xisvg <vasquezgarciaalexis@gmail.com>
2022-09-10 10:38:13 -0500

    [update] form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
   def new
     @user = User.new
+    @user.email = "jhon.doe@gmail.com"
   end
 
   def create
+    @user = User.create(email: params[:user][:email])
+    render json: @user
   end
 end
