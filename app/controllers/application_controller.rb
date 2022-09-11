2d411c41d8aca5b534c8a2e2d146a4dbb5bd45b9
al3xisvg <vasquezgarciaalexis@gmail.com>
2022-09-10 19:18:28 -0500

    [update] creating links for header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :set_links
+
+  def set_links
+    @links = [
+      { :title => "About US", :redirectTo => "/" },
+      { :title => "Contact", :redirectTo => "/" },
+    ];
+  end
 end
